7cc017a9a6840c2e8e3ee833382aea85f7a0673c
Accolades Integrated <accoladesintegrated@Accoladess-iMac.local>
2025-09-22 10:14:42 +0530

    updated with colors

--- src/app/(home)/HeroSection.jsx
+++ src/app/(home)/HeroSection.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const HeroSection = () => {
-  return (
-    <div>HeroSection</div>
-  )
-}
+  return <div className="text-[var(--color-primary)]">HeroSection</div>;
+};
 
-export default HeroSection
+export default HeroSection;
