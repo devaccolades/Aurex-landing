f98628244a7ac4e11d789216fb58c0a55cdb49ca
Afnan Accolades <ahammedafnan.accolades@gmail.com>
2025-09-25 13:03:32 +0530

    updated

--- src/app/(home)/Projects.jsx
+++ src/app/(home)/Projects.jsx
@@ -165,7 +165,7 @@ export default function Projects() {
                       setActiveProject(project);
                       setIsFormModalOpen(true);
                     }}
-                    className="flex justify-between flex-1 bg-[#006A54] text-white rounded-[10px] px-4 py-2 font-bold font-[urbanist] text-[12px] md:text-[14px]"
+                    className="flex justify-between flex-1 bg-[#006A54] text-white rounded-[10px] px-3 xl:px-4 xl:py-2 py-1 font-bold font-[urbanist] text-[12px] md:text-[14px]"
                   >
                     Enquire Now
                     <Image src={brochure} alt="brochure" />
