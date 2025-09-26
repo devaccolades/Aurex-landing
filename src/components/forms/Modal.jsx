80132b08c040cb936d056e677dad0f510e53f1e5
Afnan Accolades <ahammedafnan.accolades@gmail.com>
2025-09-26 16:38:44 +0530

    updated

--- src/components/forms/Modal.jsx
+++ src/components/forms/Modal.jsx
@@ -15,23 +15,23 @@ export default function Modal({ isOpen, onClose, children }) {
 
   return (
     <div
-      className="fixed  inset-0 flex items-center justify-center bg-black/50 z-50"
-      onClick={onClose} // click outside
+      className="fixed inset-0 flex items-center justify-center bg-black/50 z-50 px-2 md:px-4"
+      onClick={onClose}
     >
       <div
-        className="bg-white rounded-2xl w-[95%] mx-auto md:w-full max-w-lg p-2 relative "
-        onClick={(e) => e.stopPropagation()} // stop inside clicks
+        className="bg-white rounded-2xl w-full max-w-lg max-h-[90vh] overflow-y-auto relative"
+        onClick={(e) => e.stopPropagation()}
       >
         {/* Close Button */}
         <button
           onClick={onClose}
-          className="absolute z-10  text-base top-3 rounded-2xl right-[25px] cursor-pointer font-bold text-white"
+          className="absolute z-10 text-base top-3 rounded-2xl right-[25px] cursor-pointer font-bold text-white"
         >
           ✕
         </button>
 
         {/* Dynamic Content (forms) */}
-        <div className="text-base text-left">{children}</div>
+        <div className="text-base text-left p-2">{children}</div>
       </div>
     </div>
   );
